512cff863fff7b90d443460bc14bc37071aee38c
John Lam <44221419+JohnLam1992@users.noreply.github.com>
2020-07-19 16:33:43 +1000

    Initial commit

---
+++ example02/js/numeric-variable.js
@@ -0,0 +1,10 @@
+var proce;
+var quantity;
+var total;
+
+price =  5;
+quantity = 14;
+total = price * quantity;
+
+var el = document.getElementById('cost');
+el.textContent = '$' + total;
---
+++ example02/js/string-variable.js
@@ -0,0 +1,10 @@
+var username;
+var message; 
+username = 'Molly';
+message = 'See our up coming range';
+
+var elName = document.getElementById('name');
+elName.textContent = username;
+
+var elNote = document.getElementById('note');
+elNote.textContent = message;
